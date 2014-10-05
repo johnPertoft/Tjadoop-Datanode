6ddad7197609f35a43e56809e8c7cf3e92c662d1
John Pertoft <john@pertoft.se>
2014-10-05 15:40:11 +0200

    skeleton for local storage

---
+++ Datanode/src/tjadoop/LocalStorage.java
@@ -0,0 +1,12 @@
+package tjadoop;
+
+public class LocalStorage {
+
+  public LocalStorage() {
+
+  }
+
+  public void save(byte[] bytes, int offs, int len) {
+    // save len bytes from offset or until end of array
+  }
+}
