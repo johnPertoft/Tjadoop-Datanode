5e550f38c88166024a750839cb33b8dca96189c8
John Pertoft <john@pertoft.se>
2014-10-13 09:44:41 +0200

    maven

---
+++ Datanode/src/main/java/tjadoop/DatanodeServerThread.java
@@ -0,0 +1,218 @@
+package tjadoop;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class DatanodeServerThread implements Runnable {
+
+  private Datanode datanode;
+  private Socket socket;
+  private DataInputStream dis;
+  private DataOutputStream dos;
+
+  public DatanodeServerThread(Datanode datanode, Socket socket) throws IOException {
+    this.datanode = datanode;
+    this.socket = socket;
+
+    dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+    dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+  }
+
+  @Override
+  public void run() {
+
+    // implements the protocol defined in TODO
+
+    try {
+      byte requestType = dis.readByte();
+      int fileHash = dis.readInt();
+
+      switch (requestType) {
+        case DatanodeProtocol.CREATE:
+          createRequest(fileHash);
+          break;
+
+        case DatanodeProtocol.READ:
+          readRequest();
+          break;
+
+        case DatanodeProtocol.ACK:
+
+          break;
+
+        case DatanodeProtocol.DELETE:
+
+          break;
+      }
+    } catch (IOException e) {
+      // TODO: signal to Datanode that this was a malformed request or whatever happened
+      return;
+    }
+  }
+
+  private class NodeEntry {
+    byte[] iaddr;
+    long byteStart;
+    long byteEnd;
+
+    NodeEntry(byte[] iaddr, long bs, long be) {
+      this.iaddr = iaddr;
+      this.byteStart = bs;
+      this.byteEnd = be;
+    }
+  }
+
+  // TODO: this could need some refactoring maybe
+  private void createRequest(int fileHash) throws IOException {
+    short numNodes = dis.readShort();
+
+    List<NodeEntry> nodeEntries = new LinkedList<NodeEntry>();
+
+    // the byte start and end for this datanode
+    long byteStart = 0;
+    long byteEnd = 0;
+
+    for (int i = 0; i < numNodes; i++) {
+      byte[] iaddr = new byte[16];
+      dis.read(iaddr);
+      long bs = dis.readLong();
+      long be = dis.readLong();
+
+      if (isThisNodesIP(iaddr)) {
+        byteStart = bs;
+        byteEnd = be;
+
+      } else {
+        nodeEntries.add(new NodeEntry(iaddr, bs, be));
+      }
+    }
+
+    boolean isLastNode = nodeEntries.isEmpty();
+
+    Socket nextNodeSocket = null;
+    DataInputStream nextNodeDis = null;
+    DataOutputStream nextNodeDos = null;
+
+    if (!isLastNode) {
+      byte[] iaddr = nodeEntries.get(0).iaddr;
+      nextNodeSocket = new Socket(InetAddress.getByAddress(iaddr), Datanode.PORT);
+      nextNodeDis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+      nextNodeDos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+
+    // write to next node
+    if (!isLastNode) {
+      nextNodeDos.writeByte(DatanodeProtocol.CREATE);
+      nextNodeDos.writeShort(nodeEntries.size());
+      for (NodeEntry ne : nodeEntries) {
+        nextNodeDos.write(ne.iaddr, 0, ne.iaddr.length);
+        nextNodeDos.writeLong(ne.byteStart);
+        nextNodeDos.writeLong(ne.byteEnd);
+      }
+    }
+
+    long dataLength = dis.readLong();
+
+    if (!isLastNode) {
+      nextNodeDos.writeLong(dataLength);
+    }
+
+    long totalBytesRead = 0;
+    byte[] byteBlock = new byte[65536];
+
+    if (byteStart > byteEnd) {
+      // TODO: throw some exception?
+    }
+
+    String filename = fileHash + "-" + byteStart + "-" + byteEnd;
+
+    while (totalBytesRead < dataLength) { // TODO: check for EOF too
+      int bytesRead = dis.read(byteBlock);
+      totalBytesRead += bytesRead;
+
+      long currByteEnd = totalBytesRead;
+      long currByteStart = totalBytesRead - bytesRead + 1;
+
+      // if byteStart starts in this block
+      if (byteStart >= currByteStart && byteStart <= currByteEnd) {
+        int blockStart =  (int) (byteStart - currByteStart);
+
+        if (byteEnd > currByteEnd) {
+          int len = byteBlock.length - blockStart;
+          LocalStorage.save(filename, byteBlock, blockStart, len);
+
+        } else {
+          int len = (int) (currByteEnd - currByteStart);
+          LocalStorage.save(filename, byteBlock, blockStart, len);
+        }
+      }
+
+      // if byteEnd is in this block, should always come after the previous if
+      if (byteEnd >= currByteStart && byteStart <= currByteEnd) {
+        int len = (int) (byteEnd - currByteStart);
+        LocalStorage.save(filename, byteBlock, 0, len);
+      }
+
+      // if this whole block is within bytestart and byteend
+      if (byteStart < currByteStart && byteEnd > currByteEnd) {
+        LocalStorage.save(filename, byteBlock, 0, byteBlock.length);
+      }
+
+      // always pass it on to the next datanode if there is one
+      if (!isLastNode) {
+        nextNodeDos.write(byteBlock);
+      }
+    }
+
+    if (isLastNode) {
+      dos.writeByte(DatanodeProtocol.ACK);
+      // TODO: does anything else have to be sent?
+
+    } else {
+      // wait for ACK here
+      byte ack = nextNodeDis.readByte();
+      // TODO: read more stuff if we add that to protocol
+      nextNodeDis.close();
+      nextNodeDos.close();
+      nextNodeSocket.close();
+
+      // TODO: should an ack be sent to the client?
+      // TODO: if this was the first datanode in the chain we should also send an ack to namenode
+      dos.writeByte(DatanodeProtocol.ACK);
+    }
+
+    dis.close();
+    dos.close();
+  }
+
+  private void readRequest() throws IOException {
+    // TODO
+  }
+
+  private void deleteRequest() throws IOException {
+    // TODO
+  }
+
+  private void acknowledge() throws IOException {
+    // TODO
+  }
+
+  private boolean isThisNodesIP(byte[] iaddr) {
+    if (iaddr.length != datanode.IADDRESS.length) {
+      return false;
+    }
+
+    for (int i = 0; i < iaddr.length; i++) {
+      if (iaddr[i] != datanode.IADDRESS[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
---
+++ Datanode/src/main/java/tjadoop/Main.java
@@ -0,0 +1,94 @@
+package tjadoop;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class Main {
+
+  private final static String USAGE = "Need address and port to the Namenode";
+
+  private static String namenodeAddress = "";
+  private static int namenodePort;
+
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println(USAGE);
+      System.exit(1);
+    }
+
+    namenodeAddress = args[0];
+    try {
+      namenodePort = Integer.parseInt(args[1]);
+    } catch (NumberFormatException nfe) {
+      System.exit(1);
+    }
+
+    Socket socket = null;
+    ServerSocket serverSocket = null;
+
+    boolean nnSocketEstablished = false;
+    try {
+      socket = new Socket(namenodeAddress, namenodePort);
+      nnSocketEstablished = true;
+
+    } catch (IOException e) {
+      System.err.println("Failed to setup socket to namennode");
+      System.exit(1);
+    }
+
+    boolean serverSocketEstablished = false;
+    try {
+      serverSocket = new ServerSocket(Datanode.PORT);
+      serverSocketEstablished = true;
+
+    } catch (IOException e) {
+      System.err.println("Failed to setup datanodes server socket");
+      System.exit(1);
+    }
+
+    if (nnSocketEstablished && serverSocketEstablished) {
+      try {
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+        byte[] iaddr = getIP();
+        if (iaddr == null) {
+          System.err.println("Failed to get ip address of this host.");
+          System.exit(1);
+        }
+
+        System.out.println("Starting Tjadoop-Datanode");
+        new Datanode(dis, dos, serverSocket, iaddr).run();
+
+        socket.close();
+
+      } catch (IOException e) {
+        System.err.println("Failed to setup input or output stream");
+        System.exit(1);
+      }
+    }
+  }
+
+  private static byte[] getIP() throws SocketException {
+    Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+    for (NetworkInterface netint : Collections.list(nets)) {
+      if (netint.getDisplayName().startsWith("eth") || netint.getDisplayName().startsWith("wlan")) {
+        // TODO: how to choose the "right" one?
+        // TODO: prefer ipv4?
+
+        /*
+        for (InetAddress ia : Collections.list(netint.getInetAddresses())) {
+
+        }
+        */
+        return Collections.list(netint.getInetAddresses()).get(0).getAddress();
+      }
+    }
+
+    return null;
+  }
+}
